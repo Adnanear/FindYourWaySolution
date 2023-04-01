00b5400680d8b912ba22be65c7d5ec8c52ed0612
Adnane Aref <adnane.jr2021@gmail.com>
2023-04-01 22:47:30 +0200

    Feat: Product Model/Controller & Migrations

---
+++ FindYourWay/Models/Dto/ProductDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FindYourWay.Models.Dto
+{
+    public class ProductDto
+    {
+
+        public int Id { get; set; }
+
+        [StringLength(maximumLength: 125, ErrorMessage = "Name must not exceed 125 charcters long.")]
+        [Required]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Range(0, 1000000, ErrorMessage = "Price must be between 0 to 1000000.")]
+        public float Price { get; set; }
+
+        public bool Available { get; set; }
+
+        [Url(ErrorMessage = "Product image must be a valid url.")]
+        public string? Image { get; set; }
+    }
+}
---
+++ FindYourWay/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace FindYourWay.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public float Price { get; set; }
+
+        public bool Available { get; set; }
+
+        public string? Image { get; set; }
+
+    }
+}
